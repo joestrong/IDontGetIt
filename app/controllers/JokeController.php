077f9d0be5c656a25c87e9931aaa0b684c0f4ea2
Joe <joestrong@gmail.com>
2013-08-21 00:48:26 +0100

    Got new joke creation working successfully

--- app/controllers/JokeController.php
+++ app/controllers/JokeController.php
@@ -9,7 +9,23 @@ class JokeController extends BaseController{
 
     public function postCreate()
     {
-        return 'Created joke';
+        $input = Input::all();
+        $joketext = $input['joke'];
+        $explanation = $input['explanation'];
+        $validator = Validator::make(
+            array('joke' => $joketext),
+            array('joke' => 'required|min:5')
+        );
+        if($validator->passes()){
+            $joke = new Joke;
+            $joke->joke = $joketext;
+            $joke->explanation = $explanation;
+            $joke->user_id = Auth::user()->id;
+            $joke->save();
+            return Redirect::to('/')->with('message', 'Successfully added a new joke!');
+        }else{
+            return Redirect::to('/jokes/create')->withErrors($validator);
+        }
     }
 
 }
